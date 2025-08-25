a28e3e30d63978f13e6331a7259e19de70dd9b5a
Makros-24 <mohamed.el.abed.24@gmail.com>
2025-08-25 20:43:49 +0100

    chore: reset project

--- src/app/web/src/components/EnhancedParticles.tsx
+++
@@ -1,6 +0,0 @@
-// This file has been removed to fix multiple Three.js instances warning.
-// All particle functionality is now consolidated in SmoothScrollCanvas.tsx
-
-export function EnhancedParticles() {
-  return null;
-}
